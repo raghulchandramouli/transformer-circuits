bab991ae1902360245d1dd5eaaaac191f17e6664
raghulchandramouli <raghulchandramouli@gmail.com>
2026-06-27 17:53:06 +0530

    add: implement SinusoidalEncoding class in model.py

--- circuits/models/model.py
+++ circuits/models/model.py
@@ -13,4 +13,18 @@ class SinusodialEncoding(nn.Module):
 
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        
+        pe = torch.zeros(max_len, d_model)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+
+        x = x = self.pe[:x.size(1)].unsqueeze(0)
+        return self.dropout(x)
+    
+    
